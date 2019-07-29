7d32fdbe8c8ba41befbcdd2369f2de4985f1e093
Sávio Muniz <savio.muniz@vtex.com>
2019-07-29 17:47:40 -0300

    starts recaptcha

--- react/index.js
+++ react/index.js
@@ -2,12 +2,51 @@ import React, { Component } from 'react'
 import styles from './index.css'
 
 class YesNoApp extends Component {
+  constructor(props) { 
+    super(props)
+    this.state = {
+      scriptLoaded: false
+    }
+
+    this.callback.bind(this)
+  }
+
+  componentWillMount = () => {
+    this.injectScript()
+  }
+
   sayYes = () => {
     $(window).trigger('transactionValidation.vtex', [true])
   }
 
   sayNo = () => {
     $(window).trigger('transactionValidation.vtex', [false])
+  }
+
+  handleOnLoad = () => {
+    console.log("alo");
+    this.setState({scriptLoaded: true});
+  };
+
+
+  injectScript = () => {
+    if (document.getElementById('google-recaptcha-v2')) {
+      return
+    }
+
+    const head = document.getElementsByTagName('head')[0]
+
+    const js = document.createElement('script')
+    js.id = 'google-recaptcha-v2'
+    js.src = 'https://www.google.com/recaptcha/api.js'
+    js.onload = this.handleOnLoad
+
+    head.appendChild(js)
+  }
+  
+  callback = (e) => { 
+    e.preventDefault();
+    console.log(e)
   }
 
   render() {
@@ -17,8 +56,15 @@ class YesNoApp extends Component {
 
     return (
       <div className={styles.yesNoWrapper}>
-        <button onClick={this.sayYes}>Yes</button>
-        <button onClick={this.sayNo}>No</button>
+        {
+          this.state.scriptLoaded ? 
+            <div 
+            className="g-recaptcha"
+            callback='callback'
+            data-sitekey="6LeELrAUAAAAAJPKPpnuV-kf4mG8MlHHs6BPEyUj"
+            ></div> 
+            : 
+        <h1>Deu ruim :(</h1>}
       </div>
     )
   }
